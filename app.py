00ed0e0b849b01012b98bac924abc56fc585d2fc
Richard Li <lichard49@gmail.com>
2020-01-02 14:51:38 -0800

    Import socket.io for realtime server-client communication

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
-def hello_world():
+def index_page():
 	return render_template('index.html')
 
+@app.route('/sockets')
+def sockets_page():
+	return render_template('sockets.html')
+
+@socketio.on('connect')
+def on_connect():
+	print('connected!')
+	emit('message', {'data': 'Connected!'})
+
+@socketio.on('disconnect')
+def on_disconnect():
+	print('disconnected...')
+
+@socketio.on('message')
+def on_message(message):
+	print('received:', message)
+
 if __name__ == '__main__':
-	app.run()
+	socketio.run(app)
